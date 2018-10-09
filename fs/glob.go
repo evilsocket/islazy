defc749ed40e18a3b2742e1d66185fe07e4d7a06
evilsocket <evilsocket@gmail.com>
2018-10-10 01:54:43 +0200

    new: fs package

---
+++ fs/glob.go
@@ -0,0 +1,21 @@
+package fs
+
+import (
+	"path/filepath"
+)
+
+// Glob enumerates files on a given path using a globbing expression and
+// execute a callback for each of the files. The callback can interrupt
+// the loop by returning an error other than nil.
+func Glob(path string, expr string, cb func(fileName string) error) error {
+	if files, err := filepath.Glob(filepath.Join(path, expr)); err != nil {
+		return err
+	} else {
+		for _, fileName := range files {
+			if err := cb(fileName); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
