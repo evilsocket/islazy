beaa57c3cedfb22040961050d27af4e8500ba51e
evilsocket <evilsocket@gmail.com>
2018-10-12 13:39:52 +0200

    misc: small fix or general refactoring i did not bother commenting

--- examples/plugin.go
+++ examples/plugin.go
@@ -6,32 +6,35 @@ import (
 	"github.com/evilsocket/islazy/plugin"
 )
 
-var defines = map[string]interface{}{
-	"log": func(s string) interface{} {
-		fmt.Println(s)
-		return nil
-	},
-}
-
 func main() {
-	plug, err := plugin.Load("examples/plugin.js", defines)
+	// define some functions
+	plugin.Defines = map[string]interface{}{
+		"log": func(s string) interface{} {
+			fmt.Println(s)
+			return nil
+		},
+	}
+
+	// load the plugin
+	plug, err := plugin.Load("examples/plugin.js")
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
 
+	// call methods
 	methods := plug.Methods()
 	for _, m := range methods {
 		ret, err := plug.Call(m)
 		if err != nil {
 			fmt.Printf("error while calling %s function: %v\n", m, err)
 		} else if ret != nil {
-			fmt.Printf("%s returned %v\n", m, ret)
+			fmt.Printf("plugin.%s -> %v\n", m, ret)
 		}
 
 	}
 
+	// get variables
 	obj, err := plug.GetObject("Text")
 	fmt.Printf("plugin.Text = '%s'\n", obj.(string))
-
 }
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -10,6 +10,10 @@ import (
 	"github.com/robertkrimen/otto"
 )
 
+// Defines is a map containing the predefined objects
+// and functions for each vm of each plugin.
+var Defines = map[string]interface{}{}
+
 // Plugin is an object representing a javascript
 // file exporting functions and variables that
 // your project can use to extend its functionalities.
@@ -23,14 +27,13 @@ type Plugin struct {
 	Path string
 
 	vm        *otto.Otto
-	defines   map[string]interface{}
 	callbacks map[string]otto.Value
 	objects   map[string]otto.Value
 }
 
 // Load loads and compiles a plugin given its path with the
 // provided definitions.
-func Load(path string, defines map[string]interface{}) (*Plugin, error) {
+func Load(path string) (*Plugin, error) {
 	raw, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -40,7 +43,6 @@ func Load(path string, defines map[string]interface{}) (*Plugin, error) {
 		Name:      strings.Replace(filepath.Base(path), ".js", "", -1),
 		Code:      string(raw),
 		Path:      path,
-		defines:   defines,
 		callbacks: make(map[string]otto.Value),
 		objects:   make(map[string]otto.Value),
 	}
@@ -49,7 +51,7 @@ func Load(path string, defines map[string]interface{}) (*Plugin, error) {
 		return nil, err
 	}
 
-	for name, val := range defines {
+	for name, val := range Defines {
 		if err := plugin.vm.Set(name, val); err != nil {
 			return nil, err
 		}
@@ -60,7 +62,7 @@ func Load(path string, defines map[string]interface{}) (*Plugin, error) {
 
 // Clone returns a new instance identical to the plugin.
 func (p *Plugin) Clone() *Plugin {
-	clone, err := Load(p.Path, p.defines)
+	clone, err := Load(p.Path)
 	if err != nil {
 		panic(err) // this should never happen
 	}
