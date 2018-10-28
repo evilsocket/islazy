c15f6adcbd955a94d8271ee31cfc72fd8d35bd78
evilsocket <evilsocket@gmail.com>
2018-10-28 12:33:40 +0100

    new: zip.Files

--- examples/zip.go
+++ examples/zip.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	if err := zip.Files("archive.zip", []string{"README.md", "release.sh"}); err != nil {
+		panic(err)
+	}
+
 	if files, err := zip.Unzip("archive.zip", "./dest"); err != nil {
 		panic(err)
 	} else {
